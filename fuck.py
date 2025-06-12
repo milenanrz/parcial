81b9628bc6a385d59e8a83929a655ed20187a732
Sala 8 <sala8@FI-LabSistemas.ucatolica.loc>
2025-06-12 09:10:38 -0500

    Termine el CRUD o FUCK

--- fuck.py
+++ fuck.py
@@ -1,6 +1,37 @@
-from models import *
-from database import Base
+from watchfiles import awatch
 
-class Usuario(Base):
-    __tablename__="usuarios"
-    id = column (string, primar)
+from models import Usuarios
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+async  def create_user(session: AsyncSession, datos: dict):
+    nuevo_usuario = Usuarios(**datos)
+    session.add(nuevo_usuario)
+    await session.commit()
+    await session.refresh(nuevo_usuario)
+    return nuevo_usuario
+
+async def all_users(session: AsyncSession):
+    result = await session.execute(select(Usuarios). where(Usuarios, Mascota = True))
+    return result.scalars(). all()
+
+async def obtener_por_id(session: AsyncSession, id: int):
+    return await session. get(Usuarios, id)
+
+async def find_name(session: AsyncSession, nombre: dict):
+    resultado = await session.execute(select((Usuarios). where(Usuarios.nombre_usuario.ilike(f"%{nombre}"))))
+    return resultado.scalars().all()
+
+async def update_user(session: AsyncSession, id: int, datos: dict):
+    user = await obtener_por_id(session, id)
+    for k, v in datos.items():
+        setattr(user, k,v)
+    await session.commit()
+    return user
+
+async def kill_user(session: AsyncSession, id: int):
+    user = await obtener_por_id(session, id)
+    user.mascota = True
+    await session.commit()
+    return user
+
